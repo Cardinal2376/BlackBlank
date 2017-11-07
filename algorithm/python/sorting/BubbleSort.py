0a84de104af9be27704e90719768a4bd65af3ba4
Cardinal2376 <cardinal2376@gmail.com>
2017-11-07 22:56:33 +0800

    set default author name to Anonymous

--- algorithm/python/sorting/BubbleSort.py
+++ algorithm/python/sorting/BubbleSort.py
@@ -1,28 +1,28 @@
-# by wyj
-# for test py
 from tracer import *
 import random
 
 A = []
-for i in range(10):
+n = 12
+for i in range(n):
     A.append(random.randint(1, 100))
-tracer = Array1DTracer("A1D").setData(A)
-logger = LogTracer("Lt")
-A[0] = 100
-tracer.notify(0, A[0]).wait(1)
-tracer.denotify(0)
-A[1] = 1
-tracer.notify(1, A[1]).wait(1)
-for i in range(9):
-    for j in range(9 - i):
-        tracer.select(j)
-        tracer.select(j + 1).wait(1)
+chart = ChartTracer("Chart")
+tracer = Array1DTracer("Array1D").setData(A)
+tracer.attach(chart.id)
+logger = LogTracer("Logger")
+logger.Print("the origin array : " + str(A))
+for i in range(n - 1):
+    tracer.select(n - 1 - i)
+    for j in range(n - 1 - i):
+        tracer.select(j).wait()
         if A[j] > A[j + 1]:
             tmp = A[j + 1]
             A[j + 1] = A[j]
             A[j] = tmp
             tracer.notify(j, A[j])
-            tracer.notify(j + 1, A[j + 1]).wait(1)
-#for i in range(10):
-    #print(A[i])
+            tracer.notify(j + 1, A[j + 1]).wait()
+            tracer.denotify(j)
+            tracer.denotify(j+1)
+        tracer.deselect(j)
+    tracer.deselect(n - 1 - i)
+logger.Print("the sorted array : " + str(A))
 tracerlist.info()
